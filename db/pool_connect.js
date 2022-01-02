2d4e9eb04efd5571f726b18ab6ce95e0be162418
gopaljainiitr <52127546+gopaljainiitr@users.noreply.github.com>
2022-01-02 17:49:55 +0530

    Modularize code for connecting to database

---
+++ db/pool_connect.js
@@ -0,0 +1,16 @@
+const pool = require("./pool_setup");
+pool.connect((err, client, release) => {
+    if (err) {
+        return console.error(
+            'Error acquiring client', err.stack)
+    }
+    client.query('SELECT NOW()', (err, result) => {
+        release()
+        if (err) {
+            return console.error(
+                'Error executing query', err.stack)
+        }
+        console.log("Connected to Database !")
+    })
+})
+module.exports = pool;
